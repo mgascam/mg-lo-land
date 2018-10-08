22206ad92151210d39a867dea10fd232abb26643
Miguel Gasca <mgasca@theworkshop.com>
2018-10-08 23:25:47 +0200

    Latest results title

--- src/components/EuroJackpotResults.js
+++ src/components/EuroJackpotResults.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import LatestResults from './LatestResults';
+import results from '../results.json';
 
 class EuroJackpotResults extends Component {
     constructor(props) {
         super(props);
-        
+
         this.state = {
             results: null
         };
     }
+    componentDidMount() {
+        this.setState({
+            results
+        })
+    }
     render() {
         return (
-            <LatestResults results={this.state.result} />
+            <LatestResults results={this.state.results} />
         );
     }
 
