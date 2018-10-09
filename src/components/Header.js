00155d5eb72158eacb9ac2836963d38922d3e729
Miguel Gasca <mgasca@theworkshop.com>
2018-10-09 18:16:24 +0200

    Header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Header.css';
 
 export default () => {
     return (
-        <header>
+        <header className="header">
             <h1 className="title">EUROJACKPOT RESULTS & WINNING NUMBERS</h1>
             <hr />
         </header>
